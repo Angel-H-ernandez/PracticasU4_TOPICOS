ec725025412cf4cf8d6ea1c229996cb4e62a7e74
Angel Hernandez <al20020489@itsa.edu.mx>
2022-05-15 16:54:34 -0500

    upgrade DB.sql

--- DB.sql
+++ DB.sql
@@ -1,6 +1,12 @@
 -h localhost -u root -p;
-123456;
-CREATE DATABASE Practicas;
-use Practicas;
+--host localhost user root paswoord 123456
+CREATE DATABASE DBPracticasU4;
+use DBPracticasU4;
 CREATE TABLE alumno();
-
+CREATE TABLE alumno(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Calif1 INT,
+    Calif2 INT,
+    Calif3 INT,
+    Promedio FLOAT);
